0c97138a9e889c233231b48f0418e222e1424877
ivan-liljeqvist <ivanli@kth.se>
2015-04-12 22:53:25 +0200

    initial commit

---
+++ matching.go
@@ -0,0 +1,56 @@
+/*
+* author - Stefan Nilsson
+* Ivan Liljeqvist answered the questions at the end of this file.
+* The questions are in Swedish.
+*
+ */
+
+// http://www.nada.kth.se/~snilsson/concurrency/
+
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// This programs demonstrates how a channel can be used for sending and
+// receiving by any number of goroutines. It also shows how  the select
+// statement can be used to choose one out of several communications.
+func main() {
+	people := []string{"Anna", "Bob", "Cody", "Dave", "Eva"}
+	match := make(chan string, 1) // Make room for one unmatched send.
+	wg := new(sync.WaitGroup)
+	wg.Add(len(people))
+	for _, name := range people {
+		go Seek(name, match, wg)
+	}
+	wg.Wait()
+	select {
+	case name := <-match:
+		fmt.Printf("No one received %s's message.\n", name)
+	default:
+		// There was no pending send operation.
+	}
+}
+
+// Seek either sends or receives, whichever possible, a name on the match
+// channel and notifies the wait group when done.
+func Seek(name string, match chan string, wg *sync.WaitGroup) {
+	select {
+	case peer := <-match:
+		fmt.Printf("%s sent a message to %s.\n", peer, name)
+	case match <- name:
+		// Wait for someone to receive my message.
+	}
+	wg.Done()
+}
+
+/*
+
+Vad händer om man tar bort go-kommandot från Seek-anropet i main-funktionen?
+
+Vad händer om man byter deklarationen wg := new(sync.WaitGroup) mot var wg sync.WaitGroup och parametern wg *sync.WaitGroup mot wg sync.WaitGroup?
+Vad händer om man tar bort bufferten på kanalen match?
+Vad händer om man tar bort default-fallet från case-satsen i main-funktionen?
+*/
